691f4ce3d6dc9350d2b3694852f1fd9b9f813cce
Joshua Larsen <jlarsen@usgs.gov>
2021-01-20 18:38:20 -0800

    updates for filtering interpolated data

--- Work/Josh/code/create_wu_maps.py
+++ Work/Josh/code/create_wu_maps.py
@@ -1,17 +1,19 @@
 import os
+import numpy as np
+from scipy.interpolate import griddata
 import utilities as utl
 import spatial_outlier_detection as sod
-
-
-def dummy(ddf):
-    return ddf
+import matplotlib.pyplot as plt
 
 
 miles_to_m = 1.60934 * 1000
 radius = 100 * miles_to_m
 ws = os.path.abspath(os.path.dirname(__file__))
+us_shapefile = os.path.join(ws, "..", "data", "US_nation_2010_alb83.shp")
 wsa_locations = os.path.join(ws, "..", "data", "WSA_v2_1_alb83_attrib.txt")
 wsa_data = os.path.join(ws, "..", "data", "Join_swud_nswud.csv")
+
+boundary = utl.load_national_polygons(us_shapefile)
 
 df = utl.get_input_data(wsa_locations)
 df.drop(columns=["tot_wd_mgd"], inplace=True)
@@ -20,3 +22,44 @@ df1 = sod.spatial_detect(df, radius, sod.mean_stdev)
 print(df1.std_flg_1.unique())
 df1.to_csv(os.path.join(ws, "..", "data", "wu_mean_spatial_2010.csv"),
            index=False)
+
+xshape, yshape = 1500, 3000
+xv = df1.x_centroid.values
+yv = df1.y_centroid.values
+points = list(zip(xv, yv))
+
+minx = np.min(xv)
+maxx = np.max(xv)
+miny = np.min(yv)
+maxy = np.max(yv)
+
+xpts = np.linspace(minx, maxx + 1, num=xshape)
+ypts = np.linspace(miny, maxy + 1, num=yshape)
+xx, yy = np.meshgrid(xpts, ypts)
+xpts = xx.ravel()
+ypts = yy.ravel()
+
+values = df1.mean_1.values
+
+t = griddata((xv, yv), values, (xpts, ypts), method="linear")
+t.shape = (yshape, xshape)
+
+print("Starting raster intersections")
+for poly in boundary:
+    mask = utl.point_in_polygon(xx, yy, list(poly))
+    t[~mask] = np.nan
+print("Finished raster intersections")
+
+dx = abs(xx[0, 0] - xx[0, 1])
+dy = abs(yy[0, 0] - yy[1, 0])
+xxv = np.arange(minx - dx/2, maxx + 1 + dx/2, dx)
+yyv = np.arange(miny - dy/2, maxy + 1 + dy/2, dy)
+
+xxv, yyv = np.meshgrid(xxv, yyv)
+
+print('break')
+plt.pcolormesh(xxv, yyv, t)
+plt.scatter(xv, yv,)  #  'bo')
+plt.colorbar()
+plt.show()
+
--- Work/Josh/code/utilities.py
+++ Work/Josh/code/utilities.py
@@ -49,7 +49,7 @@ def get_input_data(f, dataframe=None):
 
         if "sum_gu_pop" in list(df) and "tot_wd_mgd" in list(df):
             df["wu_pp_gd"] = (df.tot_wd_mgd / df.sum_gu_pop) * 10e+6
-            df = df[df.wu_pp_mgd != 0]
+            df = df[df.wu_pp_gd != 0]
             df = df.replace([np.inf, -np.inf], 0)
         if "year" in list(df):
             df = df.loc[df.year == 2010]
@@ -58,3 +58,78 @@ def get_input_data(f, dataframe=None):
     print(len(df))
     return df
 
+
+def point_in_polygon(xc, yc, polygon):
+    """
+    Use the ray casting algorithm to determine if a point
+    is within a polygon. Enables very fast
+    intersection calculations!
+
+    Parameters
+    ----------
+    xc : np.ndarray
+        array of xpoints
+    yc : np.ndarray
+        array of ypoints
+    polygon : iterable (list)
+        polygon vertices [(x0, y0),....(xn, yn)]
+        note: polygon can be open or closed
+
+    Returns
+    -------
+    mask: np.array
+        True value means point is in polygon!
+
+    """
+    x0, y0 = polygon[0]
+    xt, yt = polygon[-1]
+
+    # close polygon if it isn't already
+    if (x0, y0) != (xt, yt):
+        polygon.append((x0, y0))
+
+    ray_count = np.zeros(xc.shape, dtype=int)
+    num = len(polygon)
+    j = num - 1
+    for i in range(num):
+
+        tmp = polygon[i][0] + (polygon[j][0] - polygon[i][0]) * (
+            yc - polygon[i][1]
+        ) / (polygon[j][1] - polygon[i][1])
+
+        comp = np.where(
+            ((polygon[i][1] > yc) ^ (polygon[j][1] > yc)) & (xc < tmp)
+        )
+
+        j = i
+        if len(comp[0]) > 0:
+            ray_count[comp[0], comp[1]] += 1
+
+    mask = np.ones(xc.shape, dtype=bool)
+    mask[ray_count % 2 == 0] = False
+
+    return mask
+
+
+def load_national_polygons(f):
+    """
+    Method to load the national boundaries from US Census into
+    polygons
+
+    Parameters
+    ----------
+    f : str
+        shapefile name
+
+    Returns
+    -------
+        list of vertices
+    """
+    import shapefile
+    t = []
+    with shapefile.Reader(f) as r:
+        for shape in r.shapes():
+            feat = shape.__geo_interface__
+            for poly in feat['coordinates']:
+                t.append(poly[0])
+    return t
