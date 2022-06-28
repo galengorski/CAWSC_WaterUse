d684471b03a26a5141397d6f1a07a6165ceff0f8
aymanalz <aalzraiee@usgs.gov>
2022-06-28 15:09:54 -0700

    more pushes...

--- ml_experiments/annual_v_0_0/1_model_development.py
+++ ml_experiments/annual_v_0_0/1_model_development.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
+# %%
 
-# In[1]:
+# %%
 
 
 import os, sys
@@ -9,14 +10,14 @@ import pandas as pd
 from pycaret.regression import *
 
 
-# In[2]:
+# %%
 
 
 dataset = pd.read_csv(r"C:\work\water_use\ml_experiments\annual_v_0_0\clean_train_db.csv")
 dataset.shape
 
 
-# In[3]:
+# %%
 
 
 data = dataset.sample(frac=0.8, random_state=786)
@@ -29,13 +30,13 @@ print('Data for Modeling: ' + str(data.shape))
 print('Unseen Data For Predictions: ' + str(data_unseen.shape))
 
 
-# In[4]:
+# %%
 
 
 exp_reg101 = setup(data = data, target = 'wu_rate', fold_shuffle= True, data_split_shuffle=True, session_id=123 )
 xx = 1
 
-# In[ ]:
+# %%
 
 
 
--- ml_experiments/annual_v_0_0/main_0_0.py
+++ ml_experiments/annual_v_0_0/main_0_0.py
@@ -1,10 +1,5 @@
 import os, sys
-sys.path.append(r"C:\work\water_use\CAWSC_WaterUse\WUtrainer")
-import data_cleaning
-import report
-from featurize import MultiOneHotEncoder
-
-
+from datetime import datetime
 import configparser
 
 import pandas as pd
@@ -13,23 +8,29 @@ from scipy import stats
 from scipy.stats import norm
 import xgboost as xgb
 import matplotlib.pyplot as plt
-#import seaborn as sns
+
 from xgboost import plot_importance
 import tensorflow as tf
 import warnings
 
-#sklearn
+# sklearn
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import FunctionTransformer
 from sklearn.compose import ColumnTransformer
 
+# local utils
+from iwateruse import data_cleaning, report
+from iwateruse.featurize import MultiOneHotEncoder
+from iwateruse import impute_utils
+
+
 class Model:
     pass
 
-def initialize(model, config_file):
 
+def initialize(model, config_file):
     model.config = configparser.ConfigParser()
-    model.config.read(r"C:\work\water_use\ml_experiments\annual_v_0_0\config_file.ini")
+    model.config.read(config_file)
 
     model.name = model.config.get("General", 'Model_Name')
     model.version = model.config.get("General", 'Version')
@@ -39,130 +40,208 @@ def initialize(model, config_file):
     model.train_file = model.config.get("Files", "Train_file")
     model.target = model.config.get("Target", "target_field")
 
-    logfile = model.config.get("Files", "Log_File")
+    logfile = model.config.get("Log", "Log_File")
+    append_date_to_log_file = model.config.get("Log", "append_date_to_file_name")
+    append_date_to_log_file = append_date_to_log_file.strip().lower()
+    if "true" in append_date_to_log_file:
+        date = datetime.now().strftime("%Y_%m_%d-%I:%M:%S_%p")
+        date = date.replace(':', "_")
+        date = date.replace('-', "_")
+        logfile = os.path.splitext(logfile)[0] + date + ".log"
     if os.path.isabs(logfile):
         abs_logfile = logfile
     else:
         abs_logfile = os.path.join(model.workspace, logfile)
-    model.log = report.Logger(abs_logfile, title = "Public Water Use")
+
+    model.log = report.Logger(abs_logfile, title="Public Water Use")
     model.log.info("initialize()...")
     model.log.info("Model Name: {}".format(model.name))
     model.log.info("Type: {}".format(model.type))
     model.log.info("Version: {}".format(model.version))
 
+
 def load(model):
-    #model.log.break_line()
+    # model.log.break_line()
     model.log.info("load()...")
 
-    feature_info_df = pd.read_excel(os.path.join(model.workspace, model.config.get('Features', 'features_info_file')))
+    model.log.info(
+        "loading: {}".format(os.path.join(model.workspace, model.config.get('Features', 'features_info_file'))))
+    feature_info_df = pd.read_excel(os.path.join(model.workspace, model.config.get('Features', 'features_info_file')),
+                                    sheet_name="annual")
     model.feature_info_df = feature_info_df
     model.features = model.feature_info_df[feature_info_df['Skip'] != 1]['Feature_name'].values.tolist()
 
-    df_main = pd.read_csv(model.train_file )
+    model.log.info("loading: {}".format(model.train_file))
+    df_main = pd.read_csv(model.train_file)
+    df_main['sample_id'] = list(df_main.index)
     df_train = df_main[df_main['wu_rate'] > 0]
     model.df_main = df_main
     model.df_train = df_train
 
     model.log.info("Finish Loading the data ...")
-
     title = 'Full annual data, shape = {}'.format(model.df_train.shape)
-    model.log.to_table(df_train, title = title)
+    model.log.to_table(df_train, title=title)
 
     summary = model.df_train.describe()
-    model.log.to_table(summary, title = "Raw Training Data Summary")
+    model.log.to_table(summary, title="Raw Training Data Summary")
+
 
 def preprocess(model):
-    #model.log.break_line()
-    model.log.info("preprocess() ...")
-
-    model.log.info("drop unused columns ...")
-    feature_mask = ~((model.feature_info_df['Skip'] == 1) | (model.feature_info_df['Not_Feature'] == 1))
-    features_to_drop = model.feature_info_df[~feature_mask]['Feature_name'].values.tolist()
-    model.df_train = model.df_train.drop(features_to_drop, axis = 1)
-    msg = ""
-    for ft in features_to_drop:
-        msg = msg + ft + ", "
-    model.log.info(" features dropped : {}".format(msg))
-
-    # make sure categorical data are integer
-    if 0: #xgboost doenot like this
-        cat_features = model.feature_info_df[model.feature_info_df['Type'] == 'categorical']['Feature_name'].values
-        for feat in cat_features:
-            if feat in model.df_train.columns:
-                model.df_train[feat] = model.df_train[feat].astype("Int64")
-
-
-    model.log.to_table(model.df_train, title= "Training data -- shape = {}".format(model.df_train.shape))
-
-
-def clean(model):
-    model.log.info("clean()...")
-
-    """ # number of establishment
-    Remove D values from Num_establishments_2017
     """
-    mask = model.df_train['Num_establishments_2017'].str.contains('D')
-    model.df_train.loc[mask.astype(bool), 'Num_establishments_2017'] = np.nan
-    model.df_train['Num_establishments_2017'] = model.df_train['Num_establishments_2017'].astype(float)
+    (1) filtering (not part of the sklearn pipeline)
+     - PC filtering
+     - Pop filtering
 
+    (2) categorical features transfromations
 
+    (3) Contenuous features transfromations
+        - StandardScaler()
 
-    """ # Summarize missing data"""
-    model.log.info("Missing data")
-    missing_df = data_cleaning.summarize_missing_data (df = model.df_train, target = model.target)
-    model.log.to_table(missing_df, title="Missing data ", header=len(missing_df))
-
-
-
-    """ Impute use XGBOOST"""
-    model.log.info("Imputing using Xgboost")
-    df_in = model.df_train.copy()
-    cat_features = model.feature_info_df[model.feature_info_df['Type'] == 'categorical']['Feature_name'].values
-    for impute_col in missing_df['index'].values:
-        flg = False
-        if impute_col in cat_features:
-            flg = True
-
-        model.log.info("Imputing {}".format(impute_col))
-        imputed_feature = data_cleaning.fill_missing_data_using_xgboost(df_in, impute_col, is_categorical = flg)
-        model.df_train[impute_col] = imputed_feature
-
-    model.log.info("Missing data after imputing ....")
-    missing_df = data_cleaning.summarize_missing_data(df=model.df_train, target=model.target)
-    model.log.to_table(missing_df, title="Missing data ", header=len(missing_df))
-
-    fn_clean_df = os.path.join(model.workspace, "clean_train_db.csv")
-    model.log.info("Clean database is generated at {} ....".format(fn_clean_df))
-    model.df_train.to_csv(fn_clean_df, index = False)
-
-def featurize(model):
     """
-    From on we use pipelines
+
+    pass
+
+
+def prepare(model):
+    """
+
     :param model:
     :return:
     """
     pass
 
+
+def list_to_df(clist, ncols):
+    all_columns = []
+    curr_line = []
+    for ifeat, feat in enumerate(clist):
+        if np.mod(ifeat, ncols) == 0:
+            all_columns.append(curr_line)
+            curr_line = []
+        curr_line.append(feat)
+
+    if len(curr_line) <= ncols:
+        clen = len(curr_line)
+        all_columns.append(curr_line + [np.nan] * (ncols - clen))
+
+    return pd.DataFrame(all_columns)
+
+
+def clean(model):
+    model.log.info("clean()...")
+    model.log.info("Columns in feature-statues file are ...")
+    dff = list_to_df(model.feature_info_df['Feature_name'].values, 5)
+    model.log.to_table(dff, header=len(dff))
+
+    features_not_in_data_base = set(model.feature_info_df['Feature_name']).difference(set(model.df_train.columns))
+    dff = list_to_df(list(features_not_in_data_base), 5)
+    model.log.to_table(dff, header=len(dff), title="Columns not in the database ...")
+
+    features_not_in_feature_list = set(model.df_train.columns).difference(set(model.feature_info_df['Feature_name']))
+    dff = list_to_df(list(features_not_in_feature_list), 5)
+    model.log.to_table(dff, header=len(dff), title="Columns not in the features list ...")
+
+    feature_mask = ~((model.feature_info_df['Skip'] == 1))# | (model.feature_info_df['Not_Feature'] == 1))
+    not_features = model.feature_info_df[~(model.feature_info_df['Not_Feature'] == 1)]['Feature_name'].values.tolist()
+    features_to_drop = model.feature_info_df[~feature_mask]['Feature_name'].values.tolist()
+    features_to_drop = (set(features_to_drop).union(features_not_in_feature_list)).difference(features_not_in_data_base)
+    features_to_drop = list(features_to_drop)
+
+    model.df_train = model.df_train.drop(features_to_drop, axis=1)
+    msg = ""
+    for ft in features_to_drop:
+        msg = msg + ft + ", "
+    model.log.info(" features dropped : {}".format(msg))
+    model.log.to_table(model.df_train, title="Training data -- shape = {}".format(model.df_train.shape))
+
+    """ # Summarize missing data"""
+    model.log.info("Missing data")
+    missing_df = data_cleaning.summarize_missing_data(df=model.df_train, target=model.target)
+    model.log.to_table(missing_df, title="Missing data ", header=len(missing_df))
+
+    """ Impute use XGBOOST"""
+    model.log.info("Imputing ...")
+    model.impute_method = model.config.get('Features', 'Impute_method')
+    model.impute_method = model.impute_method.lower()
+    if model.impute_method in ["none"]:
+        model.log.info("No imputing is made ...")
+
+
+    elif model.impute_method in ['iterative']:
+        # need some work - drop strings, insure
+        X = impute_utils.iterative_imputer(df=model.df_train.copy())
+
+    elif model.impute_method in ['xgboost']:
+        model.log.info("Imputing using Xgboost")
+        df_in = model.df_train.copy()
+        cat_features = model.feature_info_df[model.feature_info_df['Type'] == 'categorical']['Feature_name'].values
+        for impute_col in missing_df['index'].values:
+            if impute_col in not_features:
+                continue
+
+            flg = False
+            if impute_col in cat_features:
+                flg = True
+
+            model.log.info("Imputing {}".format(impute_col))
+            imputed_feature = data_cleaning.fill_missing_data_using_xgboost(df_in, impute_col, is_categorical=flg)
+            model.df_train[impute_col] = imputed_feature
+
+    model.log.info("Missing data after imputing ....")
+    missing_df = data_cleaning.summarize_missing_data(df=model.df_train, target=model.target)
+    model.log.to_table(missing_df, title="Missing data ", header=len(missing_df))
+
+    fn = model.config.get('Files', 'cleaned_training_file')
+    fn_clean_df = os.path.join(model.workspace, fn)
+    model.log.info("Clean database is generated at {} ....".format(fn_clean_df))
+    model.df_train.to_csv(fn_clean_df, index=False)
+
+
+def featurize(model):
+    """
+    From now on we use pipelines
+    :param model:
+    :return:
+    """
+    pass
+
+
 def split(model):
     pass
 
-def main():
 
-    #$ [1]
+def validate(model):
+    pass
+
+
+def make_pipeline(model):
+    pass
+
+
+def main(config_file):
+    #
     model = Model()
-    config_file = r"C:\work\water_use\ml_experiments\annual_v_0_0\config_file.ini"
     initialize(model, config_file)
     load(model)
-    preprocess(model)
 
-    use_clean_file = True
-    if use_clean_file:
-        fn_clean_df = os.path.join(model.workspace, "clean_train_db.csv")
-        model.log.info("Clean database is readed from {} ....".format(fn_clean_df))
-        model.df_train.to_csv(fn_clean_df, index=False)
+    # preprocess(model)
 
+    model.clean_file_name = os.path.abspath(model.config.get("Files", "cleaned_training_file"))
+
+    clean_the_data = model.config.get("Filter Data", "clean_data")
+    clean_the_data = clean_the_data.lower()
+    if clean_the_data in ['true']:
+        clean_the_data = True
     else:
+        clean_the_data = False
+
+    if clean_the_data:
         clean(model)
+    else:
+        model.log.info("No data cleaning is made. A cleaned file is used ... ")
+        fn_clean_df = pd.read_csv(model.clean_file_name)
+        model.log.info("Clean database is read from {} ....".format(model.clean_file_name))
+        model.df_train = fn_clean_df
 
     featurize(model)
 
@@ -170,8 +249,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
-
-
-
-
+    main(config_file=r"config_file2.ini")
